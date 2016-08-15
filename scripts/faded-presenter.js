6b0764ed7601448ff72940c9d6255360217ffdf8
myTerminal <team.terminal@gmail.com>
2016-08-15 16:19:22 +0530

    implement a basic slide-show

--- scripts/faded-presenter.js
+++ scripts/faded-presenter.js
@@ -1,16 +1,67 @@
 var FadedPresenter = function (element, htmlText, options) {
     var nice = null,
+        slideNumber = 0,
+        title,
 
         init = function () {
-            element.html(htmlText);
+            element.html(getSlideArray());
+
+            title = $("h1").html();
+            $("title").html(title);
+
+            bindEvents();
+            addLastSlide(title);
         },
 
-        nextPage = function () {
-            // TODO: Implement
+        nextSlide = function () {
+            if (slideNumber + 1 === $("div.slide").length) {
+                return;
+            }
+
+            slideNumber++;
+            showSlide();
         },
 
-        previousPage = function () {
-            // TODO: Implement
+        previousSlide = function () {
+            if (!slideNumber) {
+                return;
+            }
+
+            slideNumber--;
+            showSlide();
+        },
+
+        getSlideArray = function () {
+            var bracedString = "<div class='slide visible'>" +
+                    htmlText.replace(/\<h2/g,
+                                     "</div><div class='slide'><h2") +
+                    "</div>";
+            return bracedString;
+        },
+
+        showSlide = function () {
+            $("div.slide").
+                removeClass("visible").
+                eq(slideNumber).
+                addClass("visible");
+        },
+
+        bindEvents = function () {
+            element.on("keydown", function (e) {
+                if (e.keyCode === 39) {
+                    nextSlide();
+                } else if (e.keyCode === 37) {
+                    previousSlide();
+                }
+            });
+        },
+
+        addLastSlide = function (title) {
+            element.append("" +
+                           "<div class='slide'>" +
+                           "  <h1>" + title + "</h1>" +
+                           "  Thanks for attending the session. Questions please..." +
+                           "</div>");
         };
 
     options = options || {};
@@ -18,7 +69,7 @@ var FadedPresenter = function (element, htmlText, options) {
 
     return {
         init: init,
-        nextPage: nextPage,
-        previousPage: previousPage
+        nextSlide: nextSlide,
+        previousSlide: previousSlide
     };
 };
